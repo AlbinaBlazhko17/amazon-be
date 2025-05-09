cc8ba497c2da72ff668a597eeac54d7bd9700bc2
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-05-09 11:12:53 +0300

    feat: add global prefix and enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	app.setGlobalPrefix('/api');
+
+	app.enableCors({
+		origin: process.env.CORS_ORIGIN,
+		credentials: true,
+		exposedHeaders: ['set-cookie']
+	});
+
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
