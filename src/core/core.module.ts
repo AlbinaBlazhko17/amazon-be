c9f228a7c971a3ca7088e172dfa90791e4606d44
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-05-09 11:26:01 +0300

    feat: add core module with exception filter and logger

---
+++ src/core/core.module.ts
@@ -0,0 +1,12 @@
+import { Global, Module } from '@nestjs/common';
+
+import { ExceptionsModule } from './exceptions/exceptions.module';
+import { LoggingModule } from './logging/logging.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Global()
+@Module({
+	imports: [PrismaModule, LoggingModule, ExceptionsModule],
+	exports: [PrismaModule, LoggingModule, ExceptionsModule]
+})
+export class CoreModule {}
