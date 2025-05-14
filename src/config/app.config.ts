cc060c4e158573a38a5f6369e46772d84db71695
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-05-14 14:49:58 +0300

    feat: add variables to app config and to .env.example

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,6 +2,10 @@ export default () => ({
 	environment: process.env.NODE_ENV || 'development',
 	port: process.env.PORT || 3000,
 	database_url: process.env.DATABASE_URL,
+	refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+	jwtSecret: process.env.JWT_SECRET,
+	jwtExpiration: process.env.JWT_EXPIRATION,
+	jwtRefreshExpiration: process.env.JWT_REFRESH_EXPIRATION,
 	redis: {
 		host: process.env.REDIS_HOST || 'localhost',
 		port: parseInt(process.env.REDIS_PORT || '6379', 10),
